18d86a8b4df0cb5ee86dee90859ee9bfb94e9831
Luc Fueston <superluc@gmail.com>
2019-03-19 14:09:52 -0500

    add iri format

--- lib/ex_json_schema/validator/format.ex
+++ lib/ex_json_schema/validator/format.ex
@@ -7,6 +7,12 @@ defmodule ExJsonSchema.Validator.Format do
   @ipv4_regex ~r/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
   @ipv6_regex ~r/^(?:(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}|(?=(?:[A-F0-9]{0,4}:){0,7}[A-F0-9]{0,4}$)(([0-9A-F]{1,4}:){1,7}|:)((:[0-9A-F]{1,4}){1,7}|:)|(?:[A-F0-9]{1,4}:){7}:|:(:[A-F0-9]{1,4}){7})$/i
 
+  @iri_gruber_v2 ~r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'u
+  @iri_stephenhay ~r'^(https?|ftp)://[^\s/$.?#].[^\s]*$'ui
+  @iri_diegoperini ~r'^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$'iu
+
+  @iri_regex @iri_diegoperini
+  
   @spec validate(String.t, ExJsonSchema.data) :: Validator.errors_with_list_paths
   def validate(format, data) when is_binary(data) do
     do_validate(format, data)
@@ -32,6 +38,10 @@ defmodule ExJsonSchema.Validator.Format do
 
   defp do_validate("ipv6", data) do
     validate_with_regex(data, @ipv6_regex, fn data -> "Expected #{inspect(data)} to be an IPv6 address." end)
+  end
+
+  defp do_validate("iri", data) do
+    validate_with_regex(data, @iri_regex, fn data -> "Expected #{inspect(data)} to be an IRI." end)
   end
 
   defp do_validate(_, _) do
